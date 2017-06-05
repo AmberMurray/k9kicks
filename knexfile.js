fc15f9f220830e49b86bf921146f38104fe73eda
Amber Murray <amber.applebee@gmail.com>
2017-06-05 11:35:56 -0700

    added knexfile and config info

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/k9kicks',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
+}
