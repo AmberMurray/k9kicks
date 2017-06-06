b0982ace68afccb8fbd6e7afe11272b57dccc78a
Erica Rottman <erica7650@gmail.com>
2017-06-06 12:04:53 -0700

    update details page with bettys code

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -30,23 +30,50 @@ class ProductDetail extends Component {
   }
 
   render() {
+    const thumb =
+        <Col xs={6} md={4} >
+          <Thumbnail src={this.state.product.image_url} alt="200x200">
+            <h3>{this.state.product.product_name}</h3>
+            <p>{this.state.product.description}</p>
+            <p>${this.state.product.price}</p>
+            <span>
+              <DropdownButton title="qty" id="bg-vertical-dropdown-2">
+                <MenuItem eventKey="1">Dropdown link</MenuItem>
+              </DropdownButton>
+            </span>
+            <p>
+              <Button bsStyle="primary">Add to cart</Button>&nbsp;
+            </p>
+          </Thumbnail>
+        </Col>
+      })
 
     return (
-      <div className="Product">
-        <Header />
-        <h1>Hello</h1>
-        <h1>{this.state.product.product_name}</h1>
-        <p>{this.state.product.description}</p>
-
-
-
-          <div key={this.state.product.product_id}>{this.state.product.product_name}</div>
-          <div>  <Footer /></div>
-
-      </div>
-
-
+      <Grid>
+        <Row>
+          {thumb}
+        </Row>
+      </Grid>
     );
   }
 }
+
+//     return (
+//       <div className="Product">
+//         <Header />
+//         <h1>Hello</h1>
+//         <h1>{this.state.product.product_name}</h1>
+//         <p>{this.state.product.description}</p>
+//
+//
+//
+//           <div key={this.state.product.product_id}>{this.state.product.product_name}</div>
+//           <div>  <Footer /></div>
+//
+//       </div>
+//
+//
+//     );
+//   }
+// }
 export default ProductDetail;
