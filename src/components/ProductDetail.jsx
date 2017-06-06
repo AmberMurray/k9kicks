6c5f1ecf0e8f57bcd78dd4e42ea7272bcd988e69
Erica Rottman <erica7650@gmail.com>
2017-06-06 12:24:31 -0700

    working product details page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Image, Grid, Row, Col, Thumbnail, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import '../App.css';
 import axios from 'axios';
+
 
 class ProductDetail extends Component {
   constructor(props) {
@@ -28,64 +31,32 @@ class ProductDetail extends Component {
   }
 
   render() {
-    const thumb =
-        <Col xs={6} md={4} >
-          <Thumbnail src={this.state.product.image_url} alt="200x200">
-            <h3>{this.state.product.product_name}</h3>
-            <p>{this.state.product.description}</p>
-            <p>${this.state.product.price}</p>
-            <span>
-              <DropdownButton title="qty" id="bg-vertical-dropdown-2">
-                <MenuItem eventKey="1">Dropdown link</MenuItem>
-              </DropdownButton>
-            </span>
-            <p>
-              <Button bsStyle="primary">Add to cart</Button>&nbsp;
-            </p>
-          </Thumbnail>
-        </Col>
-      })
+    const thumb = (
+      <Col xs={6} md={4} >
+        <Thumbnail src={this.state.product.image_url} alt="200x200">
+          <h3>{this.state.product.product_name}</h3>
+          <p>{this.state.product.description}</p>
+          <p>${this.state.product.price}</p>
+          <span>
+            <DropdownButton title="qty" id="bg-vertical-dropdown-2">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+            </DropdownButton>
+          </span>
+          <p>
+            <Button bsStyle="primary">Add to cart</Button>&nbsp;
+          </p>
+        </Thumbnail>
+      </Col>
+    );
 
     return (
-
       <Grid>
         <Row>
           {thumb}
         </Row>
       </Grid>
-      //
-      // <div className="Product">
-      //   <h1>Hello</h1>
-      //   <h1>{this.state.product.product_name}</h1>
-      //   <p>{this.state.product.description}</p>
-      //
-      //
-      //
-      //     <div key={this.state.product.product_id}>{this.state.product.product_name}</div>
-      //
-      // </div>
-
-
     );
   }
 }
 
-//     return (
-//       <div className="Product">
-//         <Header />
-//         <h1>Hello</h1>
-//         <h1>{this.state.product.product_name}</h1>
-//         <p>{this.state.product.description}</p>
-//
-//
-//
-//           <div key={this.state.product.product_id}>{this.state.product.product_name}</div>
-//           <div>  <Footer /></div>
-//
-//       </div>
-//
-//
-//     );
-//   }
-// }
 export default ProductDetail;
