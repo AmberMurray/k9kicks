85ff94bdd12f24ce79478a3090f71e1118ee8fc2
Betty Chempananical <bettymariam@gmail.com>
2017-06-05 18:42:22 -0700

    Added component files

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/connection');
 
-router.get('/users', (req, res, next) => {
+router.get('/', (req, res, next) => {
   knex('users')
     .where(username, password)
     .then(reviews => res.json(reviews))
