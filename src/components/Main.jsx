540dc31c30d211125b7d8201c37bd008ec5ece5e
Amber Murray <amber.applebee@gmail.com>
2017-06-06 15:58:05 -0700

    working on home page.. still ☹️

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Jumbotron, Image, Button } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 
 
 const jumbotronInstance = (
-  <Jumbotron className="col-md-6 col-md-offset-3" display="inline-block">
-    <h1 className="text-center">Hello, world!</h1>
-    <Image className="home-image" src="/images/chihuahua-shoes.jpg" responsive />
-    <p><Button bsStyle="primary">Learn more</Button></p>
-  </Jumbotron>
+  <div className="container-fluid nerddog-pic">
+
+
+  </div>
 );
 
 
