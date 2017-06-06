5e639baf1dc3b7359decf38788e49447fee10750
Amber Murray <amber.applebee@gmail.com>
2017-06-06 14:12:36 -0700

    main image added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { ResponsiveEmbed  } from 'react-bootstrap';
+import { Jumbotron, Image, Button } from 'react-bootstrap';
 
 
-const responsiveEmbedInstance = (
-  <div className='container'>
-  <div className='col-md-6 col-md-offset-3'>
-    <ResponsiveEmbed a16by9>
-      <embed type="image/svg+xml" src="http://aprilpug.com/wp-content/uploads/2014/03/Dog-Shoes.jpg" />
-    </ResponsiveEmbed>
-  </div>
-  </div>
+const jumbotronInstance = (
+  <Jumbotron className="col-md-6 col-md-offset-3" display="inline-block">
+    <h1 className="text-center">Hello, world!</h1>
+    <Image className="home-image" src="/images/chihuahua-shoes.jpg" responsive />
+    <p><Button bsStyle="primary">Learn more</Button></p>
+  </Jumbotron>
 );
-
 
 
 class Main extends Component {
   render() {
     return (
-      responsiveEmbedInstance
-    );
+      jumbotronInstance
+    )
   }
 }
 export default Main;
