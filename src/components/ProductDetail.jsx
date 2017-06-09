9ff5052e02f8fb325396695b11c67c85a24f29bc
Amber Murray <amber.applebee@gmail.com>
2017-06-09 09:48:36 -0700

    fixed missing Link comp on productdetail page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router'
 import { Grid, Row, Col, Thumbnail, Button, DropdownButton, MenuItem,FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 import '../App.css';
 import axios from 'axios';
