1f0b2c31c75687c72163e8933d046eaa8259842d
Amber Murray <amber.applebee@gmail.com>
2017-06-10 11:16:08 -0700

    fixed home page button issues

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ class Main extends Component {
     return (
       <div className='main'>
         <section className="home-image">
-          <div className='home-nav col-md-2 col-md-offset-9'>
+          <div className='home-nav col-md-2 col-md-offset-9 col-xs-6 col-xs-offset-3'>
             <h1 className="home-name">k9kicks</h1>
             <h1 className="home-tagline">Look Sharp</h1>
             <Link className="home-button-link" to="/products"><Button className="home-button btn btn-lg">Shop</Button></Link>
